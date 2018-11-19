ec5fc4ccb149ae2c1742d7898d40eec4cdfecdf2
zjwda2016 <zjwda2016@gmail.com>
2018-11-19 09:07:18 -0500

    update

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Hello ITC-136"
+
+echo "Editted on github"
+
+echo "This is your current working directory:"
+pwd
+
+echo "This is the version of linux you are running:"
+uname -a
+
+echo "These are your logged in users:"
+who
+
